1f08fcbc3ed5ed153ded384fe65fc7fa69834674
eliya <eliyatzanani188@gmail.com>
2024-03-30 23:12:02 +0300

    add an components of home page and login page.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent},
+    { path: 'login', component: LoginComponent }
+    // { path: 'contact', component: ContactComponent },
+];
